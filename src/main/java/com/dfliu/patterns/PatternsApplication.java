7b715334f5171227a9fc744ca07f4841d3301f31
liudengfeng <liudengfeng@taoche.com>
2023-10-19 10:36:12 +0800

    更新项目pom文件

--- src/main/java/com/dfliu/patterns/PatternsApplication.java
+++ src/main/java/com/dfliu/patterns/PatternsApplication.java
@@ -1,13 +1,18 @@
 package com.dfliu.patterns;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatternsApplication {
+public class PatternsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatternsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatternsApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("启动成功!");
+    }
 }
--- src/test/java/com/dfliu/patterns/PatternsApplicationTests.java
+++ src/test/java/com/dfliu/patterns/PatternsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PatternsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
