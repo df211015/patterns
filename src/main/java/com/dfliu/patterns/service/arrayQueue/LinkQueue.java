94700fb3f810fabbad5793524afdbdc986cb20be
liudengfeng <liudengfeng@taoche.com>
2023-11-08 14:24:58 +0800

    队列的链表实现在追加尾结点时,优化了插入结点的算法,不用每次追加结点时进行遍历

--- src/main/java/com/dfliu/patterns/service/arrayQueue/LinkQueue.java
+++ src/main/java/com/dfliu/patterns/service/arrayQueue/LinkQueue.java
@@ -4,18 +4,22 @@ import com.dfliu.patterns.domain.dto.ListNode;
 
 public class LinkQueue {
     private ListNode<Integer> front;
+    /**
+     * 追加尾结点,优化了插入结点的算法,不用每次追加结点时进行遍历
+     */
+    private ListNode<Integer> rear;
 
     public void push(Integer v) {
         ListNode<Integer> newNode = this.buildNode(v);
         if (null == this.front) {
             this.front = newNode;
+            this.rear = newNode;
             return;
         }
-        ListNode<Integer> iterator = this.front;
-        while (null != iterator.getNext()) {
-            iterator = iterator.getNext();
-        }
-        iterator.setNext(newNode);
+        //将新结点挂在当前尾结点后面
+        this.rear.setNext(newNode);
+        //将尾结点指向新结点
+        this.rear = newNode;
     }
 
     public Integer pop() {
