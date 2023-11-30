afa36cfcc719fee2439c27d50ec6d3b307ef59a1
liudengfeng <liudengfeng@taoche.com>
2023-11-30 15:46:36 +0800

    追加二叉搜索树操作02,完善删除节点

--- src/main/java/com/dfliu/patterns/service/binaryTree/BinarySearchTree.java
+++ src/main/java/com/dfliu/patterns/service/binaryTree/BinarySearchTree.java
@@ -1,6 +1,7 @@
 package com.dfliu.patterns.service.binaryTree;
 
 import com.dfliu.patterns.domain.dto.TreeNode;
+import lombok.val;
 
 /**
  * 二叉搜索树: 左子数节点值 < 根节点值 < 右子数节点值
@@ -59,6 +60,69 @@ public class BinarySearchTree {
             pre.setRight(node);
         } else {
             pre.setLeft(node);
+        }
+    }
+
+    /**
+     * 删除节点
+     *
+     * @param data
+     * @return
+     */
+    public void remove(Integer data) {
+        TreeNode<Integer> pre = null;
+        TreeNode<Integer> cur = this.root;
+        while (null != cur) {
+            pre = cur;
+            if (cur.getData().equals(data)) {
+                break;
+            }
+            if (cur.getData() < data) {
+                cur = cur.getRight();
+            } else {
+                cur = cur.getLeft();
+            }
+        }
+        if (null == cur) {
+            return;
+        }
+        Integer degree = this.getDegree(cur);
+        //当被删除的节点度为0或1时
+        if (degree.equals(0) || degree.equals(1)) {
+            TreeNode<Integer> child = (null != cur.getLeft()) ? cur.getLeft() : cur.getRight();
+            if (cur.equals(this.root)) {
+                this.root = child;
+            } else {
+                if (pre.getLeft().equals(cur)) {
+                    pre.setLeft(child);
+                } else {
+                    pre.setRight(child);
+                }
+            }
+        } else if (degree.equals(2)) {
+            //当被删除的节点度为2时,获取中序遍历中,cur的下一个节点
+            TreeNode<Integer> tmp = cur.getRight();
+            while (null != tmp.getLeft()) {
+                tmp = tmp.getLeft();
+            }
+            this.remove(tmp.getData());
+            cur.setData(tmp.getData());
+        }
+    }
+
+    /**
+     * 获取指定节点的度
+     *
+     * @param node
+     * @return
+     */
+    private Integer getDegree(TreeNode<Integer> node) {
+        if (null == node.getLeft() && null == node.getRight()) {
+            return 0;
+        } else if (null == node.getLeft() || null == node.getRight()) {
+            return 1;
+        } else {
+            return 2;
         }
     }
 }
