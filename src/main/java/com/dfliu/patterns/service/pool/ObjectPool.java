d66f8e631c778b5105c4309c9dd966a6e2e0870f
jzldf180 <251334456@qq.com>
2024-01-17 21:56:43 +0800

    追加对像池横式示例

---
+++ src/main/java/com/dfliu/patterns/service/pool/ObjectPool.java
@@ -0,0 +1,66 @@
+package com.dfliu.patterns.service.pool;
+
+import java.util.Hashtable;
+
+public abstract class ObjectPool<T> {
+    private Hashtable<T, ObjectStatus> pool = new Hashtable<>();
+
+    /**
+     * 初始化时创建对像,并放入到池中
+     */
+    public ObjectPool() {
+        this.pool.put(this.createObj(), new ObjectStatus());
+    }
+
+    /**
+     * 从池中取出空闲元素
+     *
+     * @return
+     */
+    public synchronized T checkOut() {
+        for (T t : this.pool.keySet()) {
+            if (this.pool.get(t).validate()) {
+                this.pool.get(t).setUsing();
+                return t;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 归还对象
+     *
+     * @param t
+     */
+    public synchronized void checkIn(T t) {
+        this.pool.get(t).setFree();
+    }
+
+    class ObjectStatus {
+        /**
+         * 占用
+         */
+        public void setUsing() {
+            //todo
+        }
+
+        /**
+         * 释放
+         */
+        public void setFree() {
+            //todo
+        }
+
+        /**
+         * 检查是否可用
+         *
+         * @return
+         */
+        public boolean validate() {
+            //todo
+            return false;
+        }
+    }
+
+    public abstract T createObj();
+}
